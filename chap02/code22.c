d9d9277fc47924a91ac20d5c3757cd9aa5dea3b7
norihitoishida <norihitoishida@gmail.com>
2021-03-27 12:45:28 +0900

    update

--- chap02/code22.c
+++ chap02/code22.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
 void main(void){
     int val;
     int *ptr = &val;
+    printf("val=%d\n", val);
+    printf("&val=%p\n", &val);
+    printf("ptr=%p\n",ptr);
     *ptr = 41;
+    printf("val=%d\n", val);
+    printf("&val=%p\n", &val);
+    printf("ptr=%p\n",ptr);
 }
